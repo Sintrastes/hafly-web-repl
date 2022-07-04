afb0e5d6c08b5f48b49ea3b418ccf66a088e00c2
Nathan Bedell <nbedell@tulane.edu>
2022-07-04 18:13:17 -0400

    Initial implementation of basic xterm.js terminal.

--- app/Main.hs
+++ app/Main.hs
@@ -4,7 +4,36 @@ module Main where
 
 import Reflex.Dom.Core
 import Language.Javascript.JSaddle.Warp
+import Language.Javascript.JSaddle
+import Data.Functor
+import Control.Concurrent
+import Control.Monad.IO.Class
+
+header :: _ => m ()
+header = do
+  el "title" $ text "Hafly REPL"
+  elAttr "meta" (
+    "name" =: "viewport" <>
+    "content" =: "width=device-width, initial-scale=1") blank
+  elAttr "script" (
+    "src" =: "https://sintrastes.github.io/demos/hafly_repl/node_modules/xterm/lib/xterm.js") blank
+  elAttr "script" (
+    "src" =: "https://sintrastes.github.io/demos/hafly_repl/main.js") blank
+  elAttr "link" (
+    "href" =: "https://sintrastes.github.io/demos/hafly_repl/node_modules/xterm/css/xterm.css" <>
+    "type" =: "text/css" <>
+    "rel" =: "stylesheet") blank
 
 main :: IO ()
-main = run 3911 $ mainWidget $
+main = run 3911 $ mainWidgetWithHead header $ do
+     postBuild <-  getPostBuild
      el "p" $ text "Hello, reflex!"
+
+     el "div" $ 
+          elAttr "div" ("id" =: "terminal") blank
+
+     prerender (pure ()) $ do
+          liftJSM $ jsg0 ("initTerm" :: String)
+          pure ()
+
+     blank
---
+++ static/main.js
@@ -0,0 +1,15 @@
+
+function initTerm() {
+    setTimeout(function() { 
+        var term = new Terminal({convertEol: true});
+        term.open(document.getElementById('terminal'));
+        term.write(' > ');
+        term.onKey(function(x) {
+            if (x.key != '\r') {
+                term.write(x.key);
+            } else {
+                term.write('\r\n > ');
+            }
+        });
+    }, 150);
+}

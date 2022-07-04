dd999cb49ef9712f775150232b21caca36df951f
Nathan Bedell <nbedell@tulane.edu>
2022-07-04 19:58:55 -0400

    Add a variation of tryShow for use in the javascript terminal.

--- app/Main.hs
+++ app/Main.hs
@@ -2,13 +2,22 @@
 
 module Main where
 
-import Reflex.Dom.Core
+import Reflex.Dom.Core hiding (Dynamic)
 import Language.Javascript.JSaddle.Warp
-import Language.Javascript.JSaddle
+import Language.Javascript.JSaddle hiding (catch)
 import Data.Functor
 import Control.Concurrent
 import Control.Monad.IO.Class
 import qualified Data.Text as T
+import Language.Hafly.Parser
+import Language.Hafly.Stdlib
+import Language.Hafly.Interpreter
+import Text.Megaparsec.Error
+import Control.Exception
+import Data.Dynamic
+import Data.MultiMap
+import Prelude hiding (lookup)
+
 
 header :: _ => m ()
 header = do
@@ -27,9 +36,47 @@ header = do
 
 main :: IO ()
 main = run 3911 $ mainWidgetWithHead header $
-     xtermJs T.toUpper
+     xtermJs processHaflyExpr
 
-xtermJs :: _ => (T.Text -> T.Text) -> m ()
+processHaflyExpr :: T.Text -> IO T.Text
+processHaflyExpr text = 
+     let expr = parseExprDef (operatorDefs base) text
+     in case expr of
+            Right (x, xDef) -> do
+                case flattenDyn <$> interpretRec base x xDef of
+                    Left s -> pure $ T.pack s
+                    Right dy -> pure ""
+            Left err -> do
+                case parseExpression (operatorDefs base) text of
+                    Left err -> pure $ T.pack $ errorBundlePretty err
+                    Right exp -> do
+                        case interpretIO base exp of
+                            Just action -> do
+                                action
+                                return ""
+                            Nothing -> do
+                                case interpret base exp of
+                                    Left s -> pure $ T.pack s
+                                    Right result -> tryShowRes base result
+
+tryShowRes :: InterpreterContext -> Dynamic -> IO T.Text
+tryShowRes ctx@InterpreterContext {..} x = catch (do
+    case toMaybe $ dispatched "show" $ lookup "show" exprDefs of
+      Nothing -> pure $ T.pack $ show x
+      Just showF -> do
+        -- Need to guard against excepions for now as
+        --  dynApply can throw.
+        -- Would be better to model this with Either in the 
+        --  future.
+        result <- catch
+            (evaluate $ maybe (pure $ T.pack $ show x) pure . fmap T.pack . asString . flattenDyn <$> flexibleDynApp showF x)
+            (\(e :: SomeException) -> pure $ Left "")
+        case result of
+          Left s -> return $ T.pack $ show x
+          Right r -> r)
+            (\(e :: SomeException) -> pure $ T.pack $ show x)
+
+xtermJs :: _ => (T.Text -> IO T.Text) -> m ()
 xtermJs processInput = do
      el "div" $ 
           elAttr "div" ("id" =: "terminal") blank
@@ -38,8 +85,8 @@ xtermJs processInput = do
           arg <- valToText $ args Prelude.!! 0
           let continuation = args Prelude.!! 1
 
-          let result = textToJSString $ 
-               processInput arg
+          result <- textToJSString <$> liftIO
+               (processInput arg)
 
           call continuation global [result]
 

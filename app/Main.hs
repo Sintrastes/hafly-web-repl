bd12448147ed56b88d5dd951b99a185cf0c47ed1
Nathan Bedell <nbedell@tulane.edu>
2022-07-04 18:54:10 -0400

    Use current text variable to keep track of what text has been entered.

--- app/Main.hs
+++ app/Main.hs
@@ -8,6 +8,7 @@ import Language.Javascript.JSaddle
 import Data.Functor
 import Control.Concurrent
 import Control.Monad.IO.Class
+import qualified Data.Text as T
 
 header :: _ => m ()
 header = do
@@ -25,15 +26,28 @@ header = do
     "rel" =: "stylesheet") blank
 
 main :: IO ()
-main = run 3911 $ mainWidgetWithHead header $ do
-     postBuild <-  getPostBuild
-     el "p" $ text "Hello, reflex!"
+main = run 3911 $ mainWidgetWithHead header $
+     xtermJs T.toUpper
 
+xtermJs :: _ => (T.Text -> T.Text) -> m ()
+xtermJs processInput = do
      el "div" $ 
           elAttr "div" ("id" =: "terminal") blank
 
+     let callback = function $ \_ _ args -> do
+          arg <- valToText $ args Prelude.!! 0
+          let continuation = args Prelude.!! 1
+
+          let result = textToJSString $ 
+               processInput arg
+
+          call continuation global [result]
+
+          pure ()
+
      prerender (pure ()) $ do
-          liftJSM $ jsg0 ("initTerm" :: String)
+          liftJSM $ jsg1 ("initTerm" :: String)
+               callback
           pure ()
 
      blank
